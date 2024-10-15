ae4bf0234c077b45da1737e9fc28f0e6a1940425
iamtejasv <tejasv0804@gmail.com>
2024-10-15 12:56:49 +0530

    add solutions for challenges 8, 9, and 10

--- levelUpChallenges.js
+++ levelUpChallenges.js
@@ -140,3 +140,52 @@ teaCollection.forEach(function (tea) {
 });
 
 console.log(availableTeas);
+
+/*
+8. Write a `forEach` loop that iterates through the array `["Berlin", "Tokyo", "Sydney", "Paris"]`.
+Skip `"Sydney"` and store the other cities in a new array named `traveledCities`.
+*/
+
+let myWorldCities = ["Berlin", "Tokyo", "Sydney", "Paris"];
+let traveledCities = [];
+
+myWorldCities.forEach((city) => {
+  if (city === "Sydney") {
+    return;
+  }
+  traveledCities.push(city);
+});
+
+console.log(traveledCities);
+
+/* 
+9. Write a `for` loop that iterates through the array `[2, 5, 7, 9]`.
+Skip the value `7` and multiply the rest by 2. Store the results in a new array named `doubledNumbers`.
+*/
+
+let myNumbers = [2, 5, 7, 9];
+let doubledNumbers = [];
+for (let i = 0; i < myNumbers.length; i++) {
+  if (myNumbers[i] === 7) {
+    continue;
+  }
+  doubledNumbers.push(myNumbers[i] * 2);
+}
+
+console.log(doubledNumbers);
+
+/*
+10. Use a `for-of` loop to iterate through the array `["chai", "green tea", "black tea", "jasmine tea", "herbal tea"]` and stop when the length of the current tea name is greater than 10. Store the teas iterated over in an array named `shortTeas`.
+*/
+
+let myTeas = ["chai", "green tea", "black tea", "jasmine tea", "herbal tea"];
+let shortTeas = [];
+
+for (const tea of myTeas) {
+  if (tea.length > 10) {
+    break;
+  }
+  shortTeas.push(tea);
+}
+
+console.log(shortTeas);
